e446f712c678d1fb6956374dcde97f6542b25945
Victor Savage <vsava3@uic.edu>
2024-12-17 13:02:57 -0600

    central directory draft

--- Sources/ziptools/ziptools.swift
+++ Sources/ziptools/ziptools.swift
@@ -1,6 +1,17 @@
 import Foundation
-public let ZIP_CD_HEADER_SIG : UInt32 = 0x02014b50
-public let EOCD: UInt32 = 0x06054b50
+/* 
+    Central directory file header signature = 0x02014b50 
+    Most of the signatures end with 0x4b50, which is stored in little-endian ordering
+    Little Endianess: Least significant byte is stored at the Lowest memory address
+
+
+*/
+public let ZIP_CD_HEADER_SIG : [UInt8] = [0x50, 0x4b, 0x01, 0x02]
+
+/* 
+    End of Central directory  signature =  0x06054b50
+*/
+public let EOCD_SIG: [UInt8] = [0x50, 0x4b, 0x05, 0x06]
 
 enum FileError: Error {
     case CannotOpenFile(path: String)
@@ -9,12 +20,68 @@ enum FileError: Error {
 
 public struct ZipArchive {
     //maybe Data structure for archive 
+    struct ZipFile{
+        var filename_length : UInt16 = 0
+        var filename : String = ""
+        var offset: UInt32 = 0
+        var extra_field_length: UInt16
+        var comment_length: UInt16
+    }
+    var files :[ZipFile] = []
+    
     public init(){}
     public func list_files(_ path :String) throws {
         let url = URL(fileURLWithPath: path)
         do {
             let data = try Data(contentsOf: url)
+            if #available(macOS 13.0, *) {
+                if data.contains(ZIP_CD_HEADER_SIG) {
+                    let start = data.firstRange(of: ZIP_CD_HEADER_SIG)!.first!
+                    let filename_length: UInt16 = data.subdata(in: (start + 28)..<(start + 30)).withUnsafeBytes { rawBuffer in
+                        var value: UInt16 = 0
+                        // Get a mutable raw buffer to 'value'
+                        withUnsafeMutableBytes(of: &value) { destBytes in
+                            destBytes.copyBytes(from: rawBuffer)
+                        }
+                        return UInt16(littleEndian: value)
+                    }
+                    let extra_field_length: UInt16 = data.subdata(in: (start + 30)..<(start + 32)).withUnsafeBytes { rawBuffer in
+                        var value: UInt16 = 0
+                        // Get a mutable raw buffer to 'value'
+                        withUnsafeMutableBytes(of: &value) { destBytes in
+                            destBytes.copyBytes(from: rawBuffer)
+                        }
+                        return UInt16(littleEndian: value)
+                    }
+                    let comment_length: UInt16 = data.subdata(in: (start + 32)..<(start + 34)).withUnsafeBytes { rawBuffer in
+                        var value: UInt16 = 0
+                        // Get a mutable raw buffer to 'value'
+                        withUnsafeMutableBytes(of: &value) { destBytes in
+                            destBytes.copyBytes(from: rawBuffer)
+                        }
+                        return UInt16(littleEndian: value)
+                    }
+                    let data = data.subdata(in: (start + 46)..<(start + 46+Int(filename_length)))
+                    let filename: String = String(data: data, encoding: .utf8)!
+                    
+
+
+                    print(filename_length)
+                    print(extra_field_length)
+                    print(comment_length)
+                    print(filename)
+                
+                     
+                
+                }
+            }else{
+                // if data.contains(EOCD_SIG){
+
+                // }
+            }
             
+            
+
             
         }catch {
            throw FileError.CannotOpenFile(path: path)
